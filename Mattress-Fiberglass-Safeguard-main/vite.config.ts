50b13d31964692d05c27ef992145c116436fbb97
ryanbellotti <40906330+ryanbellotti@users.noreply.github.com>
2026-02-09 14:06:20 -0700

    Update vite.config.ts

--- Mattress-Fiberglass-Safeguard-main/vite.config.ts
+++ Mattress-Fiberglass-Safeguard-main/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
